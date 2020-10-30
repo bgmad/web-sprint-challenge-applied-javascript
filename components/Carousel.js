4fc6176ce0a530411179ca953be0cb33142de113
Ian Luchini <iancarloluchini@gmail.com>
2020-10-30 17:04:26 -0400

    Did something with carousel.js

--- components/Carousel.js
+++ components/Carousel.js
@@ -22,3 +22,36 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+function createImageElement(url) {
+  const img = document.createElement('img');
+  img.src = url;
+  return img;
+}
+
+function getCarousel() {
+  const carsousel = document.createElement('div');
+  const leftButton = document.createElement('div');
+  const firstImage = createImageElement("https://tk-assets.lambdaschool.com/ba687af4-3a1e-43d7-87b2-f30453264c9d_mountains.jpeg");
+  const secondImage = createImageElement("https://tk-assets.lambdaschool.com/8aa075b0-67cf-47ce-9a7f-8cc9d754675d_computer.jpeg");
+  const thirdImage = createImageElement("https://tk-assets.lambdaschool.com/5b7441c6-6e4b-4feb-a4ec-8dd2eb76238a_trees.jpeg");
+  const fourthImage = createImageElement("https://tk-assets.lambdaschool.com/0b770382-d0eb-4465-8bf2-692a79fcda71_turntable.jpeg");
+  const rightButton = document.createElement('div');
+
+  carsousel.classList.add('carousel');
+  leftButton.classList.add('left-button');
+  leftButton.textContent = '<';
+  rightButton.classList.add('right-button');
+  rightButton.textContent = '>';
+
+  carsousel.appendChild(leftButton);
+  carsousel.appendChild(firstImage);
+  carsousel.appendChild(secondImage);
+  carsousel.appendChild(thirdImage);
+  carsousel.appendChild(fourthImage);
+  carsousel.appendChild(rightButton);
+
+  return carsousel;
+}
+
+document.querySelector('.carousel-container').appendChild(getCarousel());
